9e4670e0b41612d18a807b4f75cccedc67a4ac34
Suman Kumar Nath <sknath25@gmail.com>
2024-12-14 02:19:31 +0530

    Bug/sn/remove avoid static in generic error (#2)
    * Initials
    
    * method cohesion

--- src/SResult/Result.cs
+++ src/SResult/Result.cs
@@ -1,103 +1,24 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
 namespace SResult;
 
-public sealed class Result<TSuccessResult, TFailureReason>
+public static class Result
 {
-    private readonly TSuccessResult? _successResult;
-    private readonly TFailureReason? _failureReason;
-    private readonly bool _isSuccess;
-
-    private Result(bool isSuccess, TSuccessResult? successResult, TFailureReason? failureReason)
+    public static Result<TSuccessResult, TFailureReason> CreateFailureReason<TSuccessResult, TFailureReason>(TFailureReason failureReason)
     {
-        _successResult = successResult;
-        _failureReason = failureReason;
-        _isSuccess = isSuccess;
+        return failureReason;
     }
 
-    public bool IsSuccess()
+    public static Result<object, TFailureReason> CreateFailureReason<TFailureReason>(TFailureReason failureReason)
     {
-        return _isSuccess;
+        return failureReason;
     }
 
-    public bool IsSuccess([NotNullWhen(true)] out TSuccessResult? successResult)
+    public static Result<TSuccessResult, TFailureReason> CreateSuccessResult<TSuccessResult, TFailureReason>(TSuccessResult successResult)
     {
-        successResult = _successResult;
-        return _isSuccess;
+        return successResult;
     }
 
-    public bool IsSuccess([NotNullWhen(true)] out TSuccessResult? successResult, [NotNullWhen(false)] out TFailureReason? failureReason)
+    public static Result<TSuccessResult, object> CreateSuccessResult<TSuccessResult>(TSuccessResult successResult)
     {
-        successResult = _successResult;
-        failureReason = _failureReason;
-        return _isSuccess;
+        return successResult;
     }
-
-    public Result<TSuccessResult, TFailureReason> OnSuccess(Action actionOnSuccess)
-    {
-        if(actionOnSuccess == null) throw new ArgumentNullException(nameof(actionOnSuccess));
-
-        if (IsSuccess(out _))
-        {
-            actionOnSuccess();
-        }
-
-        return this;
-    }
-
-    public Result<TSuccessResult, TFailureReason> OnSuccess(Action<TSuccessResult> actionOnSuccess)
-    {
-        if(actionOnSuccess == null) throw new ArgumentNullException(nameof(actionOnSuccess));
-
-        if(IsSuccess(out var successResponse))
-        {
-            actionOnSuccess(successResponse);
-        }
-
-        return this;
-    }
-
-    public Result<TSuccessResult, TFailureReason> OnFailure(Action actionOnFailure)
-    {
-        if(actionOnFailure == null) throw new ArgumentNullException(nameof(actionOnFailure));
-
-        if (!IsSuccess(out _, out _))
-        {
-            actionOnFailure();
-        }
-
-        return this;
-    }
-
-    public Result<TSuccessResult, TFailureReason> OnFailure(Action<TFailureReason> actionOnFailure)
-    {
-        if(actionOnFailure == null) throw new ArgumentNullException(nameof(actionOnFailure));
-
-        if (!IsSuccess(out _, out var failureResponse))
-        {
-            actionOnFailure(failureResponse);
-        }
-
-        return this;
-    }
-
-    public static Result<TSuccessResult, TFailureReason> CreateSuccessResult(TSuccessResult successResult)
-    {
-        if(successResult == null) throw new ArgumentNullException(nameof(successResult));
-
-        return new Result<TSuccessResult, TFailureReason>(true, successResult, default);
-    }
-
-    public static Result<TSuccessResult, TFailureReason> CreateFailureReason(TFailureReason failureReason)
-    {
-        if (failureReason == null) throw new ArgumentNullException(nameof(failureReason));
-
-        return new Result<TSuccessResult, TFailureReason>(false, default, failureReason);
-    }
-
-    public static implicit operator Result<TSuccessResult, TFailureReason> (TSuccessResult successResult)
-        => CreateSuccessResult(successResult);
-
-    public static implicit operator Result<TSuccessResult, TFailureReason>(TFailureReason failureReason)
-        => CreateFailureReason(failureReason);
 }
